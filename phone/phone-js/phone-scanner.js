7d7c130d9a0749e3d5d06363629a3af6d3e4658f
Ida7304 <228702553+Ida7304@users.noreply.github.com>
2026-05-31 15:41:45 +0200

    Skrevet forklarende kommentarer i phone-scanner.js

--- phone/phone-js/phone-scanner.js
+++ phone/phone-js/phone-scanner.js
@@ -1,6 +1,7 @@
 "use strict";
 
 // Puslespilsbrikker
+// puzzle1, puzzle2 osv bruges til at lave qr-koderne.
 const puzzleData = {
     puzzle1: "Du fandt brik 1!",
     puzzle2: "Du fandt brik 2!",
@@ -10,46 +11,67 @@ const puzzleData = {
 
 // -------QR-SCANNER-------
 
+// Henter "start-scanner"-knappen
 const startScanBtn = document.getElementById("startScannerBtn");
 
+// Gemmer scanner objektet her scanner.start og scanner.stop
 let scanner;
-let scanned = false;
+// Tjekker om scanneren allerede køre, så den ikke startes flere gange før den er stoppet
 let scanning = false;
 
+// Åbner scanneren i sit eget "vindue" ved at skjule hjemmeskærmen
 startScanBtn.addEventListener("click", () => {
     document.getElementById("homeScreen").style.display = "none";
     document.getElementById("scannerScreen").style.display = "block";
+    // Kalder på startScanner funktionen længere nede, og starter QR-kode-scanneren
     startScanner();
 });
 
 function startScanner() {
 
+    // Tjekker om scanning = true. 
+    // Hvis den er det stopper funktionen så brugeren ikke kan starte kameraet flere gange
     if(scanning) return;
 
+    // Fortæller koden at scanneren skal placeres i "reader" i HTML
     scanner = new Html5Qrcode("reader");
+
+    // Markerer at scanneren nu kører
     scanning = true;
 
+    // Åbner kameraet så det er klar til at scanne koderne
     scanner.start(
         
+        // Gør at kameraet automatisk bruger bagkameraet på mobilen
         { facingMode: "environment" },
 
         {
+            // Betyder "analyser 10 billeder pr. sekund"
             fps: 10,
+            // Betyder at scanneren kun forsøger at scanne et område svarende til 250x250 px
             qrbox: 250
         },
 
+        // Kalder på funktionen "onScanSucces". Den fortæller hvad der skal ske når de har scannet koden
         onScanSuccess,
     );
 }
 
-
+// Her definerer vi hvad der skal ske når de har scannet en qr-kode
+// Når koden er scannet bliver "decodedText" til f.eks "puzzle1"
 function onScanSuccess(decodedText) {
+    // Kun til test i konsollen
     console.log("Scannet:", decodedText);
 
+    // Stopper qr-scanneren
+    // Når scanneren er stoppet køres koden der er skrevet i "then(() => {}"
     scanner.stop().then(() => {
+        // Fortæller koden at scanneren ikke længere kører så den igen kan startes af brugeren
         scanning = false;
     });
 
+    // Håndterer den funde QR-kode
+    // Det vil sige at der f.eks. vil stå "handlePuzzle(puzzle1)" hvis det er den der er scannet
     handlePuzzle(decodedText);
 }
 
@@ -57,21 +79,34 @@ function onScanSuccess(decodedText) {
 
 // -------POP-OP VINDUE-------
 
+// Når en qr-kode scannes med samme navn som et af objekterne i puzzleData,
+// registreres det som "id"'et i handlePuzzle(id)
 function handlePuzzle(id) {
+    // Leder efter et matchende id fra qr-koden til puzzleData
+    // Teksten bliver så opdateret til den tilsvarende brik fra puzzleData
     const text = puzzleData[id];
 
+    // Tjekker om der er scannet en tekst (navn/id) der matcher dem i puzzleData
+    // hvis teksten er undefined, null eller tom bliver betingelsen true.
     if(!text) {
+        // Hvis qr-koden ikke matcher et id i puzzleData, vises følgende besked
         alert("Ukendt QR-kode" + id);
+        // Return stopper funktionen
         return;
     }
-
+    // Hvis qr-koden matcher et id i puzzleData fortsætter funktionen og viser teksten fra popUp i HTML
     document.getElementById("popUpText").textContent = text;
+    // Viser pop-op vinduet
     document.getElementById("popUp").style.display = "block";
 }
 
-
+// Laver en funktion til at lukke pop-op vinduet igen.
+// Den er sat på som et click-event i HTML
 function closePopUp() {
+    // Lukker pop-op vinduet
     document.getElementById("popUp").style.display = "none";
+    // Viser hjemmeskærmen igen
     document.getElementById("homeScreen").style.display = "block";
+    // Skjuler scanner-skærmen
     document.getElementById("scannerScreen").style.display = "none";
 }
