573952289d3b541fcf4665de001cf6755ea7efe9
Ida7304 <228702553+Ida7304@users.noreply.github.com>
2026-05-31 11:36:49 +0200

    Lavet animation-transition fra phone.html til phone-scanner.html

--- phone/phone-js/phonescript.js
+++ phone/phone-js/phonescript.js
@@ -1,1 +1,14 @@
 "use strict";
+
+
+
+// Laver animation mellem start-siden og scanner-siden
+document.getElementById("tried-before-btn").addEventListener("click", () => {
+    document.body.classList.add("fade-out");
+
+    setTimeout(() => {
+        // window finder browser-vinduet location.href finder adressen og skifter den til den nye (phone-scanner.html)
+        window.location.href = "phone-scanner.html";
+    }, 500);
+});
+
