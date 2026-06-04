710b6699c016aa662b7ef521f59b4fbd23eb4233
miriam-r-nygaard <eaa25miny@students.eaaa.dk>
2026-06-04 16:31:00 +0200

    Updated_comments_collage1_2_3_and_tutorial

--- tablet/js-tablet/tablet-tutorial.js
+++ tablet/js-tablet/tablet-tutorial.js
@@ -1,18 +1,12 @@
 "use strict";
+// =====================================================================================================
+// Knapper
+//======================================================================================================
 // Hjem-knap: går til forsiden (index.html)
 const homeBtn = document.getElementById("homeIcon");
 if (homeBtn) {
   homeBtn.addEventListener("click", () => {
     window.location.href = "index.html";
-  });
-}
-
-// Tilbage-knap: går et skridt tilbage i browserhistorikken (som at trykke på browserens tilbage-knap)
-// tjek lige om virker senere
-const backBtn = document.getElementById("backIcon");
-if (backBtn) {
-  backBtn.addEventListener("click", () => {
-    window.history.back();
   });
 }
 
